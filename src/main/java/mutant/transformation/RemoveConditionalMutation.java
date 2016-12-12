09c41407cac157f235ae55cba824d6745b2a7a52
bdanglot <bdanglot@gmail.com>
2016-12-12 11:31:40 +0100

    add exp from exp

---
+++ src/main/java/mutant/transformation/RemoveConditionalMutation.java
@@ -0,0 +1,28 @@
+package mutant.transformation;
+
+import fr.inria.diversify.mutant.transformation.MutationTransformation;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtLiteral;
+import spoon.reflect.factory.Factory;
+
+/**
+ * User: Simon
+ * Date: 13/02/14
+ * Time: 14:45
+ */
+public class RemoveConditionalMutation extends MutationTransformation<CtExpression, CtLiteral<Boolean>> {
+
+    public RemoveConditionalMutation(CtExpression transplantationPoint) {
+        super(transplantationPoint);
+        type = "mutation";
+        name = "removeConditional";
+    }
+
+
+    @Override
+    protected void buildTransplant() {
+        Factory factory = transplantationPoint.getFactory();
+        transplant = factory.Core().createLiteral();
+        transplant.setValue(true);
+    }
+}
