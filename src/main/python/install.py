30934082fbf5b459ceb9d0e0ff8f8235c62cf964
bdanglot <bdanglot@gmail.com>
2017-01-23 09:52:29 +0100

    install script

---
+++ src/main/python/install.py
@@ -0,0 +1,17 @@
+import subprocess
+import json
+import sys
+
+project=sys.argv[1]
+
+with open('dataset/dataset.json') as data_file:
+    data = json.load(data_file)
+
+print data[project]["url"]
+
+cmd = "git clone "
+cmd += data[project]["url"]
+cmd += " dataset/"
+cmd += project
+
+subprocess.call(cmd, shell=True)
