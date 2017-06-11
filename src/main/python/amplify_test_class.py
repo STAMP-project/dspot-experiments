2a17103d1c7821e0aedf682b1ebc3be90feed67a
danglotb <bdanglot@gmail.com>
2017-06-11 15:55:11 +0200

    update scripts

---
+++ src/main/python/amplify_test_class.py
@@ -0,0 +1,29 @@
+import profiling_test_class
+import subprocess
+import sys
+
+projects = sys.argv[1:]
+
+cmd = "~/jdk1.8.0_121/jre/bin/java -Xms16G -Xmx32G" \
+      " -jar ../dspot/target/dspot-1.0.0-jar-with-dependencies.jar" \
+      " --path-to-properties src/main/resources/{}.properties" \
+      " --amplifiers MethodAdd:TestDataMutator:StatementAdderOnAssert" \
+      " --iteration 3" \
+      " --output-path {}" \
+      " --maven-home /home/spirals/danglot/apache-maven-3.3.9/" \
+      " -t {}" \
+      " -m original/per_class/{}/{}_mutations.csv"
+
+for project in projects:
+    top, worst = profiling_test_class.profile(projects=[project])
+    print cmd.format(project, project, top[0][2], project, top[0][2])
+    subprocess.call(cmd.format(project, project, top[0][2], project, top[0][2]),
+                    shell=True)
+    print cmd.format(project, project, top[1][2], project, top[1][2])
+    subprocess.call(cmd.format(project, project, top[1][2], project, top[1][2]),
+                    shell=True)
+    print cmd.format(project, project, worst[0][2], project, worst[0][2])
+    subprocess.call(cmd.format(project, project, worst[0][2], project, worst[0][2])
+                    , shell=True)
+    print cmd.format(project, project, worst[1][2], project, worst[1][2])
+    subprocess.call(cmd.format(project, project, worst[1][2], project, worst[1][2]), shell=True)
--- src/main/python/to_fullqualifiedname.py
+++ src/main/python/to_fullqualifiedname.py
@@ -12,8 +12,6 @@ prefix = "original/per_class/"
 
 projects = ["javapoet", "mybatis", "traccar", "stream-lib", "mustache.java", "twilio-java", "jsoup", "protostuff",
             "logback", "retrofit"]
-projects = ["javapoet", "traccar", "stream-lib", "mustache.java", "twilio-java", "jsoup", "protostuff", "logback",
-            "retrofit"]
 
 with open("dataset/properties_rates.json") as data_file:
     properties_rates = json.load(data_file)
@@ -22,6 +20,7 @@ prefix_properties = "src/main/resources/"
 extension_properties = ".properties"
 
 for project in projects:
+    print project
     properties = build_rate_table.load_properties(prefix_properties + project + extension_properties)
     test_directory = properties["testSrc"]
     path = prefix_dataset + project + "/" + properties_rates[project]["subModule"] + "/"
@@ -33,14 +32,17 @@ for project in projects:
                 if filename.endswith(".java") and "Test" in filename:
                     java_files.append(dirpath[len(path + "/" + test_directory):] + "/" + filename)
 
-    for (dirpath, dirnames, filenames) in walk("original/per_class/"):
+    print java_files
+
+    for (dirpath, dirnames, filenames) in walk("original/per_class/" + project):
         if filenames:
             for filename in filenames:
                 test_class_name = filename.split("_")[0]
                 for java_file in java_files:
                     if test_class_name == java_file.split(".")[0].split("/")[-1]:
-                        print "mv " + dirpath + "/" + filename + " " +  dirpath + "/" + java_file.split(".")[0].replace("/",".") + "_mutations.csv"
-
-
-
+                        cmd = "mv " + dirpath + "/" + filename + " " + dirpath + "/" + java_file.split(".")[0].replace(
+                            "/", ".") + "_mutations.csv"
+                        print cmd
+                        subprocess.call(cmd, shell=True)
+    print 30 * "="
     #    print java_file.split(".")[0].replace("/",".")
