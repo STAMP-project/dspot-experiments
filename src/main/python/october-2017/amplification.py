e76b0f484da3b7b6c9ff07389be6d068dac2dc9c
danglotb <bdanglot@gmail.com>
2017-10-18 13:40:04 +0200

    update scripts

--- src/main/python/october-2017/amplification.py
+++ src/main/python/october-2017/amplification.py
@@ -3,9 +3,16 @@ import json
 
 import build_rate_table
 
+blacklist=[
+    "com.squareup.javapoet.UtilTest",
+    "com.twilio.rest.api.v2010.account.usage.record.AllTimeTest",
+    "com.twilio.rest.api.v2010.account.usage.record.DayTimeTest",
+    "org.traccar.protocol.At2000ProtocolDecoderTest"
+]
 
 def allsame(x):
     return len(set(x)) == 1
+
 
 
 def run(project, mvnHome="~/apache-maven-3.3.9/", javaHome="~/jdk1.8.0_121/bin/", withAmplifier=True):
@@ -48,8 +55,9 @@ def run(project, mvnHome="~/apache-maven-3.3.9/", javaHome="~/jdk1.8.0_121/bin/"
     types = ["top_1", "top_2", "worst_1", "worst_2"]
     for type in types:
         java_file = selected_classes[project][type]
-        print amplify + opt_test + java_file + opt_mutations_original + prefix_original_mutation_file + java_file + "_mutations.csv"
-        print
+        if not java_file in blacklist:
+            print amplify + opt_test + java_file + opt_mutations_original + prefix_original_mutation_file + java_file + "_mutations.csv"
+            print
     #print "zip -r dspot-report.zip dspot-report"
     #print "to_download=$(curl --upload-file dspot-report.zip " + "https://transfer.sh/" + project + "_dspot-report.zip)"
     #print "echo \"curl ${to_download} -o " + project + "_dspot-report.zip\""
--- src/main/python/october-2017/mutation_analysis.py
+++ src/main/python/october-2017/mutation_analysis.py
@@ -4,6 +4,14 @@ import json
 import build_rate_table
 
 
+blacklist=[
+    "com.squareup.javapoet.UtilTest",
+    "com.twilio.rest.api.v2010.account.usage.record.AllTimeTest",
+    "com.twilio.rest.api.v2010.account.usage.record.DayTimeTest",
+    "org.traccar.protocol.At2000ProtocolDecoderTest"
+]
+
+
 def allsame(x):
     return len(set(x)) == 1
 
@@ -48,20 +56,21 @@ def run(project, mvn_home="~/apache-maven-3.3.9/bin/",  java_home="~/jdk1.8.0_12
 
     for type in types:
         java_file=selected_classes[project][type]
-        path_to_target = prefix_dataset + project + "/" + \
-                         (properties_rates[project]["subModule"] + "/" if not properties_rates[project][
-                                                                                  "subModule"] == "" else "")
-        if amplified:
-            print "cp ${root_exp}/" + prefix_results + project + "/" + build_rate_table.buildAmplTestPath(java_file) + ".java ${root_exp}/" + \
-                  path_to_target + properties["testSrc"] + build_rate_table.buildPackageAsPath(java_file) + "/"
-        print run_pitest + (fullQualifiedNameToAmplifiedName(selected_classes[project][type]) if amplified else selected_classes[project][type])
-        path_to_pit_results = prefix_dataset + project + "/" + \
-                         (properties_rates[project]["subModule"] + "/" if not properties_rates[project][
-                                                                                  "subModule"] == "" else "")
-        path_to_output_pit_results = ("results/" if amplified else "original/") + "october-2017/" + project + \
-            "/" + (fullQualifiedNameToAmplifiedName(selected_classes[project][type]) if amplified else selected_classes[project][type])
-        print "python src/main/python/october-2017/copy_pit_results.py " + path_to_pit_results + " " + path_to_output_pit_results
-        print
+        if not java_file in blacklist:
+            path_to_target = prefix_dataset + project + "/" + \
+                             (properties_rates[project]["subModule"] + "/" if not properties_rates[project][
+                                                                                      "subModule"] == "" else "")
+            if amplified:
+                print "cp ${root_exp}/" + prefix_results + project + "/" + build_rate_table.buildAmplTestPath(java_file) + ".java ${root_exp}/" + \
+                      path_to_target + properties["testSrc"] + build_rate_table.buildPackageAsPath(java_file) + "/"
+            print run_pitest + (fullQualifiedNameToAmplifiedName(selected_classes[project][type]) if amplified else selected_classes[project][type])
+            path_to_pit_results = prefix_dataset + project + "/" + \
+                             (properties_rates[project]["subModule"] + "/" if not properties_rates[project][
+                                                                                      "subModule"] == "" else "")
+            path_to_output_pit_results = ("results/" if amplified else "original/") + "october-2017/" + project + \
+                "/" + (fullQualifiedNameToAmplifiedName(selected_classes[project][type]) if amplified else selected_classes[project][type])
+            print "python src/main/python/october-2017/copy_pit_results.py " + path_to_pit_results + " " + path_to_output_pit_results
+            print
 
 if __name__ == '__main__':
 
