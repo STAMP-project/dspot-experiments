bfcee0b4a73b4eb879d06a1b3e23313837f85b06
danglotb <bdanglot@gmail.com>
2017-11-03 12:55:30 +0100

    update installation

--- src/main/bash/install_dspot.sh
+++ src/main/bash/install_dspot.sh
@@ -5,7 +5,7 @@
 #
 
 cd ..
-git clone http://github.com/STAMP-project/dspot.git
+git clone http://github.com/danglotb/dspot.git -b spoon-5.9.0
 cd dspot
 ~/apache-maven-3.3.9/bin/mvn clean package install -DskipTests
 cd ../dspot-experiments
--- src/main/bash/launch.sh
+++ src/main/bash/launch.sh
@@ -7,5 +7,5 @@ export MAVEN_HOME=~/apache-maven-3.3.9/
 ./src/main/bash/run-mutant-original.sh
 ./src/main/bash/amplification.sh
 ./src/main/bash/run-mutant-amplified.sh
-./src/main/bash/amplification-aampl.sh
-./src/main/bash/run-mutant-amplified-aampl.sh
+#./src/main/bash/amplification-aampl.sh
+#./src/main/bash/run-mutant-amplified-aampl.sh
