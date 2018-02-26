2b8cfdc9837f9da3f84198f36727f2d0ffa1c873
danglotb <bdanglot@gmail.com>
2018-02-26 17:16:58 +0100

    update scripts

---
+++ src/main/python/february-2018/copy_pit_results.py
@@ -0,0 +1,14 @@
+from os import walk
+import sys
+import subprocess
+
+def run(path_to_project, fullqualified_name):
+    path = path_to_project + "/target/pit-reports/"
+    for (dirpath, dirnames, filenames) in walk(path):
+        if filenames:
+            copycmd = "cp " + dirpath + "/mutations.csv " + fullqualified_name + "_mutations.csv"
+            subprocess.call(copycmd, shell=True)
+            return
+
+if __name__ == '__main__':
+    run(path_to_project=sys.argv[1], fullqualified_name=sys.argv[2])
