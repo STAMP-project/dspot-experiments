1d5a2a7006670274e071983d1b6c45eb551ef207
danglotb <bdanglot@gmail.com>
2018-08-02 14:40:27 +0200

    update: can pass classes to be executed throught command line

--- src/main/python/august-2018/run-original-pit.py
+++ src/main/python/august-2018/run-original-pit.py
@@ -1,32 +1,29 @@
 import toolbox
 import sys
 
-import os
-
-
-def run(project):
+def run(project, classes=toolbox.keys_selected_classes):
     path_to_output = toolbox.prefix_current_dataset + project
     selected_classes = toolbox.getTestClassesToBeAmplified(project)
     properties = toolbox.getProperties(project)
     filter = toolbox.load_properties(project)[toolbox.key_filter]
     module = properties[toolbox.key_subModule]
-    current_class = toolbox.top_1
-    toolbox.output_log_path = toolbox.getAbsolutePath(toolbox.prefix_current_dataset + project + "/" + current_class + "_original_mutations.log")
-    toolbox.printAndCall(
-        # print (
-        [toolbox.maven_home + "mvn", "clean", "test", "-DskipTests",
-         "org.pitest:pitest-maven:1.4.0:mutationCoverage",
-         "-DoutputFormats=CSV",
-         "-DtimeoutConst=10000",
-         "-DjvmArgs=16G",
-         "-DmutationEngines=gregor",
-         "-Dmutators=ALL",
-         "-DreportsDirectory=" + toolbox.getAbsolutePath(path_to_output + "/" + current_class),
-         "-DtargetClasses=" + filter,
-         "-DtargetTests=" + selected_classes[current_class],
-         "-DexcludedTestClasses=" + properties[toolbox.key_excludedClasses]
-         ], cwd=toolbox.prefix_dataset + project + module
-    )
+    for current_class in classes:
+        toolbox.output_log_path = toolbox.getAbsolutePath(toolbox.prefix_current_dataset + project + "/" + current_class + "_original_mutations.log")
+        toolbox.printAndCall(
+            # print (
+            [toolbox.maven_home + "mvn", "clean", "test", "-DskipTests",
+             "org.pitest:pitest-maven:1.4.0:mutationCoverage",
+             "-DoutputFormats=CSV",
+             "-DtimeoutConst=10000",
+             "-DjvmArgs=16G",
+             "-DmutationEngines=gregor",
+             "-Dmutators=ALL",
+             "-DreportsDirectory=" + toolbox.getAbsolutePath(path_to_output + "/" + current_class),
+             "-DtargetClasses=" + filter,
+             "-DtargetTests=" + selected_classes[current_class],
+             "-DexcludedTestClasses=" + properties[toolbox.key_excludedClasses]
+             ], cwd=toolbox.prefix_dataset + project + module
+        )
 
 
 if __name__ == '__main__':
@@ -48,4 +45,4 @@ if __name__ == '__main__':
         for project in projects:
             run(project)
     else:
-        run(sys.argv[1])
+        run(sys.argv[1], classes=sys.argv[2:])
