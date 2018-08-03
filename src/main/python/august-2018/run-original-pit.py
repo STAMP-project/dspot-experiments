e2c4a915d60d03d8e45ada7b2e3d1ed88dceee0f
danglotb <bdanglot@gmail.com>
2018-08-03 14:33:00 +0200

    add slash

--- src/main/python/august-2018/run-original-pit.py
+++ src/main/python/august-2018/run-original-pit.py
@@ -24,7 +24,7 @@ def run(project, classes=toolbox.keys_selected_classes):
              "-DtargetClasses=" + filter,
              "-DtargetTests=" + selected_classes[current_class],
              "-DexcludedTestClasses=" + properties[toolbox.key_excludedClasses]
-             ], cwd=toolbox.prefix_dataset + project + module
+             ], cwd=toolbox.prefix_dataset + project + "/" + module
         )
 
 
