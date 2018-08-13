b3bf3890c0dba133877a38045f254b46f0627f80
danglotb <benjamin.danglot@inria.fr>
2018-08-13 16:04:35 +0200

    add result for retrofit

---
+++ results/august-2018/retrofit/top_2_amplification/retrofit2/AmplCallAdapterTest.java
@@ -0,0 +1,12 @@
+package retrofit2;
+
+
+public final class AmplCallAdapterTest {
+    @SuppressWarnings("unused")
+    static class A<T> {
+        T method() {
+            return null;
+        }
+    }
+}
+
