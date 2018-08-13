8de2f15e2441afd5ed5814d292c61cba0011a278
danglotb <bdanglot@gmail.com>
2018-08-13 13:11:52 +0200

    fix: amplify script for generate new test class

--- src/main/python/august-2018/amplify.py
+++ src/main/python/august-2018/amplify.py
@@ -19,28 +19,31 @@ def run(project, classes=toolbox.keys_selected_classes):
         # print(
         path_to_output_for_class = toolbox.get_absolute_path(path_to_output + "/" + current_class + "_amplification/")
         toolbox.delete_if_exists(path_to_output_for_class)
-        cmd = " ".join(
-            [toolbox.java_home + "java", "-XX:-UseGCOverheadLimit", "-XX:-OmitStackTraceInFastThrow", "-Xms8G",
-             "-Xmx16G",
-             "-jar",
-             toolbox.path_to_dspot_jar,
-             "--path-to-properties",
-             toolbox.get_absolute_path(toolbox.prefix_resources + project + toolbox.suffix_properties),
-             "--verbose",
-             "--no-minimize",
-             "--working-directory",
-             "--output-path", path_to_output_for_class,
-             "--iteration", "3",
-	         "--budgetizer", "NoBudgetizer",
-             "--amplifiers",
-             "StringLiteralAmplifier:NumberLiteralAmplifier:CharLiteralAmplifier:BooleanLiteralAmplifier:MethodAdd:MethodRemove:MethodGeneratorAmplifier:ReturnValueAmplifier:NullifierAmplifier",
-             "--test-criterion", "PitMutantScoreSelector",
-             "--path-pit-result", path_to_original_mutation_score,
-             "--test", selected_classes[current_class],
-             "--maven-home", toolbox.maven_home
-             ]
-        ) + ("" if not selected_classes[current_class] in toolbox.no_amplified_name_classes else "--generate-new-test-class")
-        toolbox.print_and_call_in_a_file(cmd)
+        cmd = [
+            toolbox.java_home + "java", "-XX:-UseGCOverheadLimit", "-XX:-OmitStackTraceInFastThrow", "-Xms8G",
+            "-Xmx16G",
+            "-jar",
+            toolbox.path_to_dspot_jar,
+            "--path-to-properties",
+            toolbox.get_absolute_path(toolbox.prefix_resources + project + toolbox.suffix_properties),
+            "--verbose",
+            "--no-minimize",
+            "--working-directory",
+            "--output-path", path_to_output_for_class,
+            "--iteration", "3",
+            "--budgetizer", "NoBudgetizer",
+            "--amplifiers",
+            "StringLiteralAmplifier:NumberLiteralAmplifier:CharLiteralAmplifier:BooleanLiteralAmplifier:MethodAdd:MethodRemove:MethodGeneratorAmplifier:ReturnValueAmplifier:NullifierAmplifier",
+            "--test-criterion", "PitMutantScoreSelector",
+            "--path-pit-result", path_to_original_mutation_score,
+            "--test", selected_classes[current_class],
+            "--maven-home", toolbox.maven_home
+        ]
+
+        if selected_classes[current_class] in toolbox.no_amplified_name_classes:
+            cmd.append("--generate-new-test-class")
+
+        toolbox.print_and_call_in_a_file(" ".join(cmd))
 
 
 if __name__ == '__main__':
