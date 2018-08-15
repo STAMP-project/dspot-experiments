618067d9f16375cba0d0814c2be838d31c97f097
danglotb <benjamin.danglot@inria.fr>
2018-08-15 12:07:24 +0200

    add result traccar

--- results/august-2018/traccar/worst_1_amplification/org/traccar/protocol/AmplProgressProtocolDecoderTest.java
+++ results/august-2018/traccar/worst_1_amplification/org/traccar/protocol/AmplProgressProtocolDecoderTest.java
@@ -16,16 +16,27 @@ public class AmplProgressProtocolDecoderTest extends ProtocolTest {
         Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
         Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
         ByteBuf o_testDecode__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).hasMemoryAddress());
         Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).isDirect());
         Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode__4)).hasArray());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).hasMemoryAddress());
+        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode__4)).isReadable());
+        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).isWritable());
         Assert.assertEquals("UnpooledHeapByteBuf(ridx: 0, widx: 63, cap: 63/63)", ((UnpooledHeapByteBuf) (o_testDecode__4)).toString());
         Assert.assertEquals(-1557132967, ((int) (((UnpooledHeapByteBuf) (o_testDecode__4)).hashCode())));
         Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).isReadOnly());
-        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode__4)).isReadable());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode__4)).isWritable());
         Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
         Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
+    }
+
+    @Test(timeout = 10000)
+    public void testDecodelitString4_failAssert1() throws Exception {
+        try {
+            ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
+            binary("GdhscbCS@!x*zH_,y(q2 5[gpbL[{$QV5:Wz2[|+mr6#-VtX(r!Fs2l>UgIvC=TU&zgYc TM1`_8;0L`A=SO/woO!OKS@Rl&{ha!&Bcvg[?i!rb0/|]6^FT)-ef&bk");
+            org.junit.Assert.fail("testDecodelitString4 should have thrown RuntimeException");
+        } catch (RuntimeException expected) {
+            Assert.assertEquals("org.apache.commons.codec.DecoderException: Illegal hexadecimal character G at index 0", expected.getMessage());
+        }
     }
 
     @Test(timeout = 10000)
@@ -34,9 +45,9 @@ public class AmplProgressProtocolDecoderTest extends ProtocolTest {
         Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
         Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
         ByteBuf o_testDecodelitString5__4 = binary("");
+        Assert.assertTrue(((EmptyByteBuf) (o_testDecodelitString5__4)).hasMemoryAddress());
         Assert.assertFalse(((EmptyByteBuf) (o_testDecodelitString5__4)).isReadable());
         Assert.assertFalse(((EmptyByteBuf) (o_testDecodelitString5__4)).isWritable());
-        Assert.assertTrue(((EmptyByteBuf) (o_testDecodelitString5__4)).hasMemoryAddress());
         Assert.assertEquals("EmptyByteBufBE", ((EmptyByteBuf) (o_testDecodelitString5__4)).toString());
         Assert.assertEquals(1, ((int) (((EmptyByteBuf) (o_testDecodelitString5__4)).hashCode())));
         Assert.assertTrue(((EmptyByteBuf) (o_testDecodelitString5__4)).isDirect());
@@ -47,93 +58,52 @@ public class AmplProgressProtocolDecoderTest extends ProtocolTest {
     }
 
     @Test(timeout = 10000)
-    public void testDecodelitString6_failAssert4() throws Exception {
-        try {
-            ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
-            binary("\n");
-            org.junit.Assert.fail("testDecodelitString6 should have thrown RuntimeException");
-        } catch (RuntimeException expected) {
-            Assert.assertEquals("org.apache.commons.codec.DecoderException: Odd number of characters.", expected.getMessage());
-        }
-    }
-
-    @Test(timeout = 10000)
-    public void testDecodelitString5_add101() throws Exception {
-        ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
-        Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
-        Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
-        ByteBuf o_testDecodelitString5__4 = binary("");
-        ((EmptyByteBuf) (o_testDecodelitString5__4)).isReadOnly();
-        Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
-        Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
-    }
-
-    @Test(timeout = 10000)
-    public void testDecode_add8litString38_failAssert8() throws Exception {
-        try {
-            ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
-            ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb7b6");
-            ByteBuf o_testDecode_add8__5 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-            org.junit.Assert.fail("testDecode_add8litString38 should have thrown RuntimeException");
-        } catch (RuntimeException expected) {
-            Assert.assertEquals("org.apache.commons.codec.DecoderException: Odd number of characters.", expected.getMessage());
-        }
-    }
-
-    @Test(timeout = 10000)
-    public void testDecode_add8_add145_add2508() throws Exception {
+    public void testDecode_add8litString41() throws Exception {
         ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
         Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
         Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
         ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-        ByteBuf o_testDecode_add8_add145_add2508__7 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hasMemoryAddress());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isDirect());
-        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hasArray());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isWritable());
-        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isReadable());
-        Assert.assertEquals("UnpooledHeapByteBuf(ridx: 0, widx: 63, cap: 63/63)", ((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).toString());
-        Assert.assertEquals(-1557132967, ((int) (((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hashCode())));
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isReadOnly());
-        ByteBuf o_testDecode_add8_add145__7 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-        ByteBuf o_testDecode_add8__5 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+        ByteBuf o_testDecode_add8__5 = binary("");
         Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
         Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hasMemoryAddress());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isDirect());
-        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hasArray());
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isWritable());
-        Assert.assertTrue(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isReadable());
-        Assert.assertEquals("UnpooledHeapByteBuf(ridx: 0, widx: 63, cap: 63/63)", ((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).toString());
-        Assert.assertEquals(-1557132967, ((int) (((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).hashCode())));
-        Assert.assertFalse(((UnpooledHeapByteBuf) (o_testDecode_add8_add145_add2508__7)).isReadOnly());
     }
 
     @Test(timeout = 10000)
-    public void testDecodenull9_failAssert6_add124_add1656() throws Exception {
+    public void testDecode_add8litString40_failAssert16() throws Exception {
         try {
             ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
-            Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
-            Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
-            ((ProgressProtocolDecoder) (decoder)).isSharable();
-            binary(null);
-            binary(null);
-            org.junit.Assert.fail("testDecodenull9 should have thrown NullPointerException");
-        } catch (NullPointerException expected) {
-        }
-    }
-
-    @Test(timeout = 10000)
-    public void testDecode_add8_add145litString2462_failAssert31() throws Exception {
-        try {
-            ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
-            ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530[03136333832383531353535010000000100000000000000e6bb97b6");
-            ByteBuf o_testDecode_add8_add145__7 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-            ByteBuf o_testDecode_add8__5 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
-            org.junit.Assert.fail("testDecode_add8_add145litString2462 should have thrown RuntimeException");
+            ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+            ByteBuf o_testDecode_add8__5 = binary("+{5@T5!^MYU(dM7KJ&><6ycw,-c^.vZ(8(U^r,Jp9Flz5*yC=M]:bMoV#NG^1yAAF?5P&+YTN/#yO[*WW4JN-$nw<}7EGpwmm(EQndBdj-qEHp!#I]LDWP=,y4JV)d");
+            org.junit.Assert.fail("testDecode_add8litString40 should have thrown RuntimeException");
         } catch (RuntimeException expected) {
-            Assert.assertEquals("org.apache.commons.codec.DecoderException: Illegal hexadecimal character [ at index 70", expected.getMessage());
+            Assert.assertEquals("org.apache.commons.codec.DecoderException: Illegal hexadecimal character + at index 0", expected.getMessage());
         }
+    }
+
+    @Test(timeout = 10000)
+    public void testDecode_add8_add77litString564_failAssert27() throws Exception {
+        try {
+            ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
+            ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+            ByteBuf o_testDecode_add8__5 = binary("\n");
+            ((ProgressProtocolDecoder) (decoder)).getProtocolName();
+            org.junit.Assert.fail("testDecode_add8_add77litString564 should have thrown RuntimeException");
+        } catch (RuntimeException expected) {
+            Assert.assertEquals("org.apache.commons.codec.DecoderException: Odd number of characters.", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void testDecode_add8_add77_add1821() throws Exception {
+        ProgressProtocolDecoder decoder = new ProgressProtocolDecoder(new ProgressProtocol());
+        Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
+        Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
+        ((ProgressProtocolDecoder) (decoder)).getProtocolName();
+        ByteBuf o_testDecode_add8__4 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+        ByteBuf o_testDecode_add8__5 = binary("020037000100000003003131310f003335343836383035313339303036320f00323530303136333832383531353535010000000100000000000000e6bb97b6");
+        ((ProgressProtocolDecoder) (decoder)).getProtocolName();
+        Assert.assertEquals("progress", ((ProgressProtocolDecoder) (decoder)).getProtocolName());
+        Assert.assertFalse(((ProgressProtocolDecoder) (decoder)).isSharable());
     }
 }
 
--- results/august-2018/traccar/worst_2_amplification/org/traccar/events/AmplIgnitionEventHandlerTest.java
+++ results/august-2018/traccar/worst_2_amplification/org/traccar/events/AmplIgnitionEventHandlerTest.java
@@ -1,14 +1,10 @@
 package org.traccar.events;
 
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
 import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import org.traccar.BaseTest;
-import org.traccar.model.CellTower;
 import org.traccar.model.Event;
 import org.traccar.model.Network;
 import org.traccar.model.Position;
@@ -17,106 +13,47 @@ import org.traccar.model.WifiAccessPoint;
 
 public class AmplIgnitionEventHandlerTest extends BaseTest {
     @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg16_mg354() throws Exception {
-        WifiAccessPoint __DSPOT_wifiAccessPoint_121 = new WifiAccessPoint();
-        Network __DSPOT_network_8 = new Network();
+    public void testIgnitionEventHandler_mg11_mg647() throws Exception {
+        Network __DSPOT_network_227 = new Network(new org.traccar.model.CellTower());
+        double __DSPOT_course_3 = 0.1467843967493232;
         IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
         Position position = new Position();
         position.set(Position.KEY_IGNITION, true);
         position.setValid(true);
         Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-        position.setNetwork(__DSPOT_network_8);
-        __DSPOT_network_8.addWifiAccessPoint(__DSPOT_wifiAccessPoint_121);
+        position.setCourse(__DSPOT_course_3);
+        position.setNetwork(__DSPOT_network_227);
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
     }
 
     @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg14_mg855() throws Exception {
-        Network __DSPOT_network_359 = new Network(new CellTower());
-        double __DSPOT_latitude_6 = 0.10089601329744369;
+    public void testIgnitionEventHandler_add4_add268_mg4508() throws Exception {
+        Network __DSPOT_network_611 = new Network(new org.traccar.model.CellTower());
         IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
         Position position = new Position();
         position.set(Position.KEY_IGNITION, true);
+        position.set(Position.KEY_IGNITION, true);
+        position.setValid(true);
         position.setValid(true);
         Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-        position.setLatitude(__DSPOT_latitude_6);
-        position.setNetwork(__DSPOT_network_359);
+        position.setNetwork(__DSPOT_network_611);
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
     }
 
     @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg16_mg354_mg8795() throws Exception {
-        WifiAccessPoint __DSPOT_wifiAccessPoint_3181 = new WifiAccessPoint();
-        WifiAccessPoint __DSPOT_wifiAccessPoint_121 = new WifiAccessPoint();
-        Network __DSPOT_network_8 = new Network();
+    public void testIgnitionEventHandlerlitBool2_mg533_mg5473() throws Exception {
+        WifiAccessPoint __DSPOT_wifiAccessPoint_1576 = new WifiAccessPoint();
+        Network __DSPOT_network_113 = new Network();
         IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
         Position position = new Position();
         position.set(Position.KEY_IGNITION, true);
-        position.setValid(true);
+        position.setValid(false);
         Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-        position.setNetwork(__DSPOT_network_8);
-        __DSPOT_network_8.addWifiAccessPoint(__DSPOT_wifiAccessPoint_121);
-        __DSPOT_network_8.addWifiAccessPoint(__DSPOT_wifiAccessPoint_3181);
-        Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
-    }
-
-    @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg16_mg383_mg7626_failAssert22() throws Exception {
-        try {
-            WifiAccessPoint __DSPOT_wifiAccessPoint_2771 = new WifiAccessPoint();
-            Collection<WifiAccessPoint> __DSPOT_wifiAccessPoints_131 = Collections.singletonList(new WifiAccessPoint());
-            Network __DSPOT_network_8 = new Network();
-            IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
-            Position position = new Position();
-            position.set(Position.KEY_IGNITION, true);
-            position.setValid(true);
-            Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-            position.setNetwork(__DSPOT_network_8);
-            __DSPOT_network_8.setWifiAccessPoints(__DSPOT_wifiAccessPoints_131);
-            __DSPOT_network_8.addWifiAccessPoint(__DSPOT_wifiAccessPoint_2771);
-            org.junit.Assert.fail("testIgnitionEventHandler_mg16_mg383_mg7626 should have thrown UnsupportedOperationException");
-        } catch (UnsupportedOperationException expected) {
-            Assert.assertEquals(null, expected.getMessage());
-        }
-    }
-
-    @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg16_mg362_mg8059_failAssert40() throws Exception {
-        try {
-            CellTower __DSPOT_cellTower_2924 = new CellTower();
-            Collection<CellTower> __DSPOT_cellTowers_124 = Collections.<CellTower>emptyList();
-            Network __DSPOT_network_8 = new Network();
-            IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
-            Position position = new Position();
-            position.set(Position.KEY_IGNITION, true);
-            position.setValid(true);
-            Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-            position.setNetwork(__DSPOT_network_8);
-            __DSPOT_network_8.setCellTowers(__DSPOT_cellTowers_124);
-            __DSPOT_network_8.addCellTower(__DSPOT_cellTower_2924);
-            org.junit.Assert.fail("testIgnitionEventHandler_mg16_mg362_mg8059 should have thrown UnsupportedOperationException");
-        } catch (UnsupportedOperationException expected) {
-            Assert.assertEquals(null, expected.getMessage());
-        }
-    }
-
-    @Test(timeout = 10000)
-    public void testIgnitionEventHandler_mg19_mg592_mg6883() throws Exception {
-        Collection<WifiAccessPoint> __DSPOT_wifiAccessPoints_2469 = Collections.singletonList(new WifiAccessPoint());
-        Network __DSPOT_network_244 = new Network(new CellTower());
-        Date __DSPOT_serverTime_11 = new Date();
-        IgnitionEventHandler ignitionEventHandler = new IgnitionEventHandler();
-        Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
-        Position position = new Position();
-        position.set(Position.KEY_IGNITION, true);
-        position.setValid(true);
-        Map<Event, Position> events = ignitionEventHandler.analyzePosition(position);
-        position.setServerTime(__DSPOT_serverTime_11);
-        position.setNetwork(__DSPOT_network_244);
-        __DSPOT_network_244.setWifiAccessPoints(__DSPOT_wifiAccessPoints_2469);
+        position.setNetwork(__DSPOT_network_113);
+        __DSPOT_network_113.addWifiAccessPoint(__DSPOT_wifiAccessPoint_1576);
         Assert.assertTrue(((IgnitionEventHandler) (ignitionEventHandler)).isSharable());
     }
 }
