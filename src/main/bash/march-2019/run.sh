316259b81ef8498627792868ec1834ea7f504ced
danglotb <benjamin.danglot@inria.fr>
2019-02-22 15:45:35 +0100

    fix: scripts to train model

--- src/main/bash/march-2019/run.sh
+++ src/main/bash/march-2019/run.sh
@@ -9,10 +9,19 @@ git clone https://github.com/tech-srl/code2vec
 cd code2vec
 git reset --hard a7873314a1066c945048ef25f8f843e4c932c207
 
+cd ..
+cp src/main/bash/march-2019/preprocess.sh code2vec/preprocess.sh
+cp src/main/bash/march-2019/train.sh code2vec/train.sh
+
 # retrieve the raw-benchmark from Zenodo
 cd code2vec/
 wget https://zenodo.org/record/2567792/files/benchmark.zip
 unzip benchmark.zip
+
+wget https://s3.amazonaws.com/code2vec/model/java14m_model.tar.gz
+tar -xvzf java14m_model.tar.gz
+
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 # preprocess
 sh preprocess.sh
--- src/main/bash/march-2019/train.sh
+++ src/main/bash/march-2019/train.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ###########################################################
 # Change the following values to train a new model.
 # type: the name of the new model, only affects the saved file name.
@@ -5,6 +6,9 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
+
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 type=dspot-prettifier-exp-march-2019
 dataset_name=dspot-prettifier-exp-march-2019
 data_dir=data/${dataset_name}
@@ -12,6 +16,10 @@ data=${data_dir}/${dataset_name}
 test_data=${data_dir}/${dataset_name}.val.c2v
 model_dir=models/${type}
 
+# already trained model java14m
+trained_model=models/java14_model/saved_model_iter8
+
 mkdir -p models/${model_dir}
 set -e
-python3 -u code2vec.py --data ${data} --test ${test_data} --save ${model_dir}/saved_model
+#python3 -u code2vec.py --data ${data} --test ${test_data} --save ${model_dir}/saved_model
+python3 -u code2vec.py --load ${trained_model} --data ${data} --test ${test_data} --save ${model_dir}/saved_model
