67844dbbece7bcdb9de77d415270e9f1e77ab251
danglotb <benjamin.danglot@inria.fr>
2019-04-29 08:37:07 +0200

    add result commons lang input amplification 4

--- results/april-2019/commons-lang/commit_4_e7d16c2/input_amplification/3/org/apache/commons/lang3/builder/AmplJsonToStringStyleTest.java
+++ results/april-2019/commons-lang/commit_4_e7d16c2/input_amplification/3/org/apache/commons/lang3/builder/AmplJsonToStringStyleTest.java
@@ -21,24 +21,29 @@ public class AmplJsonToStringStyleTest {
     }
 
     @Test(timeout = 10000)
-    public void testArray_literalMutationString57991_add60681() throws Exception {
+    public void testArray_literalMutationString57926_add60550() throws Exception {
         final ToStringStyleTest.Person p = new ToStringStyleTest.Person();
-        p.name = "Jan/ Doe";
-        Assert.assertEquals("Jan/ Doe", p.name);
+        p.name = "<;o/V.8S";
+        Assert.assertEquals("<;o/V.8S", p.name);
         p.age = 25;
         p.smoker = true;
-        ToStringBuilder o_testArray_literalMutationString57991_add60681__6 = new ToStringBuilder(p).append("name", p.name).append("age", p.age);
-        Assert.assertEquals("{\"name\":\"Jan/ Doe\",\"age\":25,", ((StringBuffer) (((ToStringBuilder) (o_testArray_literalMutationString57991_add60681__6)).getStringBuffer())).toString());
-        Assert.assertEquals("{\"name\":\"Jan/ Doe\",\"age\":25}", ((ToStringBuilder) (o_testArray_literalMutationString57991_add60681__6)).toString());
+        ToStringBuilder o_testArray_literalMutationString57926_add60550__6 = new ToStringBuilder(p).append("name", p.name).append("age", p.age).append("smoker", p.smoker).append("groups", new Object() {
+            @Override
+            public String toString() {
+                return "['admin', 'manager', 'user']";
+            }
+        });
+        Assert.assertEquals("{\"name\":\"<;o/V.8S\",\"age\":25,\"smoker\":true,\"groups\":[\'admin\', \'manager\', \'user\'],", ((StringBuffer) (((ToStringBuilder) (o_testArray_literalMutationString57926_add60550__6)).getStringBuffer())).toString());
+        Assert.assertEquals("{\"name\":\"<;o/V.8S\",\"age\":25,\"smoker\":true,\"groups\":[\'admin\', \'manager\', \'user\']}", ((ToStringBuilder) (o_testArray_literalMutationString57926_add60550__6)).toString());
         new ToStringBuilder(p).append("name", p.name).append("age", p.age).append("smoker", p.smoker).append("groups", new Object() {
             @Override
             public String toString() {
                 return "['admin', 'manager', 'user']";
             }
         }).toString();
-        Assert.assertEquals("Jan/ Doe", p.name);
-        Assert.assertEquals("{\"name\":\"Jan/ Doe\",\"age\":25}", ((StringBuffer) (((ToStringBuilder) (o_testArray_literalMutationString57991_add60681__6)).getStringBuffer())).toString());
-        Assert.assertEquals("{\"name\":\"Jan/ Doe\",\"age\":25}}", ((ToStringBuilder) (o_testArray_literalMutationString57991_add60681__6)).toString());
+        Assert.assertEquals("<;o/V.8S", p.name);
+        Assert.assertEquals("{\"name\":\"<;o/V.8S\",\"age\":25,\"smoker\":true,\"groups\":[\'admin\', \'manager\', \'user\']}", ((StringBuffer) (((ToStringBuilder) (o_testArray_literalMutationString57926_add60550__6)).getStringBuffer())).toString());
+        Assert.assertEquals("{\"name\":\"<;o/V.8S\",\"age\":25,\"smoker\":true,\"groups\":[\'admin\', \'manager\', \'user\']}}", ((ToStringBuilder) (o_testArray_literalMutationString57926_add60550__6)).toString());
     }
 
     static class NestingPerson {
