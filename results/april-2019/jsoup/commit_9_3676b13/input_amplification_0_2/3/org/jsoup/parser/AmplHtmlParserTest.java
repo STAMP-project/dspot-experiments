aed186770ed148f848edee4d5e9430896a1c0d5e
Benjamin Danglot <bedanglot@chetemi-4.lille.grid5000.fr>
2019-05-23 16:09:47 +0200

    add result jsoup 9 it 3 s 0 2

---
+++ results/april-2019/jsoup/commit_9_3676b13/input_amplification_0_2/3/org/jsoup/parser/AmplHtmlParserTest.java
@@ -0,0 +1,43 @@
+package org.jsoup.parser;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.StringUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class AmplHtmlParserTest {
+    @Test(timeout = 10000)
+    public void handlesJavadocFont_add54430_literalMutationNumber54862_failAssert0_add59508_failAssert0() throws Exception {
+        try {
+            {
+                String h = "<TD BGCOLOR=\"#EEEEFF\" CLASS=\"NavBarCell1\">    <A HREF=\"deprecated-list.html\"><FONT CLASS=\"NavBarFont1\"><B>Deprecated</B></FONT></A>&nbsp;</TD>";
+                Document doc = Jsoup.parse(h);
+                Element a = doc.select("a").first();
+                String o_handlesJavadocFont_add54430__7 = a.text();
+                String o_handlesJavadocFont_add54430__8 = a.child(0).tagName();
+                String o_handlesJavadocFont_add54430__10 = a.child(0).child(-1).tagName();
+                a.child(0).child(0);
+                String o_handlesJavadocFont_add54430__13 = a.child(0).child(0).tagName();
+                org.junit.Assert.fail("handlesJavadocFont_add54430_literalMutationNumber54862 should have thrown ArrayIndexOutOfBoundsException");
+            }
+            org.junit.Assert.fail("handlesJavadocFont_add54430_literalMutationNumber54862_failAssert0_add59508 should have thrown ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException expected) {
+            Assert.assertEquals(null, expected.getMessage());
+        }
+    }
+
+    @Ignore
+    @Test
+    public void handlesMisnestedAInDivs() {
+        String h = "<a href='#1'><div><div><a href='#2'>child</a</div</div></a>";
+        String w = "<a href=\"#1\"></a><div><a href=\"#1\"></a><div><a href=\"#1\"></a><a href=\"#2\">child</a></div></div>";
+        Document doc = Jsoup.parse(h);
+        Assert.assertEquals(StringUtil.normaliseWhitespace(w), StringUtil.normaliseWhitespace(doc.body().html()));
+    }
+}
+
