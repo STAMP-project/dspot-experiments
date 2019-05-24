435433340cef1a329de210929e01c5ae94a3e5a2
Benjamin Danglot <bedanglot@chetemi-3.lille.grid5000.fr>
2019-05-24 11:05:24 +0200

    add result jsoup 9 seeds 12 14

---
+++ results/april-2019/jsoup/commit_9_3676b13/seeds/733434_12_14/org/jsoup/parser/AmplAttributeParseTest.java
@@ -0,0 +1,46 @@
+package org.jsoup.parser;
+
+
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.BooleanAttribute;
+import org.jsoup.nodes.Element;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AmplAttributeParseTest {
+    @Test(timeout = 10000)
+    public void parsesBooleanAttributes_add156() throws Exception {
+        String html = "<a normal=\"123\" boolean empty=\"\"></a>";
+        Element el = Jsoup.parse(html).select("a").first();
+        String o_parsesBooleanAttributes_add156__6 = el.attr("normal");
+        Assert.assertEquals("123", o_parsesBooleanAttributes_add156__6);
+        String o_parsesBooleanAttributes_add156__7 = el.attr("boolean");
+        Assert.assertEquals("", o_parsesBooleanAttributes_add156__7);
+        String o_parsesBooleanAttributes_add156__8 = el.attr("empty");
+        Assert.assertEquals("", o_parsesBooleanAttributes_add156__8);
+        List<Attribute> attributes = el.attributes().asList();
+        int o_parsesBooleanAttributes_add156__12 = attributes.size();
+        Assert.assertEquals(3, ((int) (o_parsesBooleanAttributes_add156__12)));
+        boolean boolean_134 = (attributes.get(0)) instanceof BooleanAttribute;
+        Attribute o_parsesBooleanAttributes_add156__15 = attributes.get(1);
+        Assert.assertEquals("boolean=\"\"", ((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).toString());
+        Assert.assertEquals(2006063320, ((int) (((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).hashCode())));
+        Assert.assertNull(((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).getValue());
+        Assert.assertEquals("boolean", ((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).getKey());
+        boolean boolean_135 = (attributes.get(1)) instanceof BooleanAttribute;
+        boolean boolean_136 = (attributes.get(2)) instanceof BooleanAttribute;
+        el.outerHtml();
+        Assert.assertEquals("123", o_parsesBooleanAttributes_add156__6);
+        Assert.assertEquals("", o_parsesBooleanAttributes_add156__7);
+        Assert.assertEquals("", o_parsesBooleanAttributes_add156__8);
+        Assert.assertEquals(3, ((int) (o_parsesBooleanAttributes_add156__12)));
+        Assert.assertEquals("boolean=\"\"", ((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).toString());
+        Assert.assertEquals(2006063320, ((int) (((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).hashCode())));
+        Assert.assertNull(((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).getValue());
+        Assert.assertEquals("boolean", ((BooleanAttribute) (o_parsesBooleanAttributes_add156__15)).getKey());
+    }
+}
+
