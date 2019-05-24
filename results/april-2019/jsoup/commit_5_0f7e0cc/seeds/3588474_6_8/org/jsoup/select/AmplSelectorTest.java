fc19f6672814dc936a1a8b4f49bc0dbe4e6faef0
Benjamin Danglot <bedanglot@chetemi-8.lille.grid5000.fr>
2019-05-24 08:33:23 +0200

    jsoup 5 seed 6 8

---
+++ results/april-2019/jsoup/commit_5_0f7e0cc/seeds/3588474_6_8/org/jsoup/select/AmplSelectorTest.java
@@ -0,0 +1,43 @@
+package org.jsoup.select;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.MultiLocaleRule;
+import org.jsoup.nodes.Document;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class AmplSelectorTest {
+    @Rule
+    public MultiLocaleRule rule = new MultiLocaleRule();
+
+    @Test(timeout = 10000)
+    public void testPseudoHas_literalMutationNumber5357_failAssert0() throws Exception {
+        try {
+            Document doc = Jsoup.parse("<div id=0><p><span>Hello</span></p></div> <div id=1><span class=foo>There</span></div> <div id=2><p>Not</p></div>");
+            Elements divs1 = doc.select("div:has(span)");
+            divs1.size();
+            divs1.get(0).id();
+            divs1.get(1).id();
+            Elements divs2 = doc.select("div:has([class])");
+            divs2.size();
+            divs2.get(-1).id();
+            Elements divs3 = doc.select("div:has(span, p)");
+            divs3.size();
+            divs3.get(0).id();
+            divs3.get(1).id();
+            divs3.get(2).id();
+            Elements els1 = doc.body().select(":has(p)");
+            els1.size();
+            els1.first().tagName();
+            els1.get(1).id();
+            els1.get(2).id();
+            org.junit.Assert.fail("testPseudoHas_literalMutationNumber5357 should have thrown ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException expected) {
+            Assert.assertEquals("-1", expected.getMessage());
+        }
+    }
+}
+
